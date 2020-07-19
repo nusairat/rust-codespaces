74a6c2e651033550a3873e2cb57cdd6febe766a1
Joseph Faisal Nusairat <jnusairat@integrallis.com>
2020-07-19 21:36:57 +0000

    #comment updated

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 
 Containers were based on : https://github.com/second-state/learn-rust-with-github-actions/blob/master/.devcontainer/devcontainer.json
+
+Test with :
+
+curl -i http://localhost:3000/healthz
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use iron::{Iron, Request, Response, IronResult, status};
+use iron::prelude::Chain;
+use router::{Router};
+use router::router;
+use mount::Mount;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!");    
+
+    let chain = Chain::new(health_routes());
+
+    let mut mount = Mount::new();
+    mount.mount("/", chain);
+
+
+    // create the handler and bind it it to a port
+    println!("Run :: http://localhost:3000/healthz");
+
+    Iron::new(mount).http("localhost:3000");
+    
+}
+
+pub fn health(_: &mut Request) -> IronResult<Response> {
+    println!("Health Check ...");
+    Ok(Response::with((status::Ok, "OK")))
+}
+
+fn health_routes() -> Router {
+    router!(
+        health: get "/healthz" => health
+    )
 }
